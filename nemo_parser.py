ac3f44176b286503d161e862e43ba589c3fd8288
Juanma-Sanchez <juanmanuel.sanchez@telcaria.com>
2016-12-23 16:33:30 +0100

    Added functionality to get nodes from intent (still missing properties and subnodes).

--- nemo_parser.py
+++ nemo_parser.py
@@ -2,17 +2,37 @@ class Nemo_Intent:
 
     sentences = []
     objects = []
+    operations = []
+
+    def __init__(self):
+        self.objects = Nemo_objects()
 
 class Nemo_objects:
 
     nodes = []
     flows = []
-    operations = []
+    connections = []
+
+    def append_Node(self, name, node_type, subnodes=None, properties=None):
+        self.nodes.append(Nemo_Node(name, node_type, subnodes, properties))
 
 class Nemo_Node:
 
-    node_type = []
+    name = None
+    node_type = None
     sub_nodes = []
+    properties = []
+
+    def __init__(self, name, node_type, subnodes=None, properties=None):
+        self.name = name
+        self.node_type = node_type
+        if subnodes:
+            self.sub_nodes = subnodes
+        if properties:
+            self.properties = properties
+    def __str__(self):
+        string = "name: " + self.name + '\n' + "type: " + self.node_type
+        return string
 
 def parse_Intent(intent_file):
     Intent = Nemo_Intent()
@@ -26,5 +46,20 @@ def parse_Intent(intent_file):
     while '' in sentences:
         sentences.remove('')
     Intent.sentences = sentences
+
+    #Get network nodes
+    for sentence in Intent.sentences:
+        sentence = sentence.replace(',','')
+        sentence = sentence.replace(';','')
+        words = sentence.split(' ')
+        #print words
+        if words[0] == 'CREATE' or words[0] == 'IMPORT':
+            if words[1] == 'Node':
+                name = words [2]
+                node_type = words[4]
+                if len(words) > 5:
+                    if words[5] == 'Property':
+                        print "property"
+                Intent.objects.append_Node(name, node_type)       
 
     return Intent
--- test_parser.py
+++ test_parser.py
@@ -4,3 +4,8 @@ from nemo_parser import *
 
 Intent = parse_Intent(sys.argv[1])
 print Intent.sentences
+for node in Intent.objects.nodes:
+    print
+    print "Node:"
+    print node
+
